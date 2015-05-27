fe366b4203f9794cd83ac4e05922bfcc67cdef9f
Tak1s <deedee604@gmail.com>
2015-05-28 01:30:17 +0300

    add modules

--- chat/app.js
+++ chat/app.js
@@ -1,78 +1,59 @@
 var express = require('express');
-
+var connect = require('connect');
+var errorhandler = require('errorhandler');
 var http = require('http');
 var path = require('path');
-var app = express();
+var config = require('config');
+var log = require('libs/log')(module);
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-app.set('port', 3000)
+var app = express();
+app.set('port', config.get('port'))
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listing on port ' + app.get('port'));
+    log.info('Express server listing on port ' + config.get('port'));
 })
 
-app.use(function(req, res){
-    res.end('Hello!');
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res, next){
+    res.render('chat_page',{});
+});
+//app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res) {
+    res.send(404, "Page not found. Sorry")
 });
 
 
-// development error handler
-// will print stacktrace
+app.use(function(err, req, res, next) {
+    if (app.get('env') === 'development') {
+        errorhandler(err, req, res, next);
+    }else{
+        app.send(500);
+    }
+});
+if (app.get('env') === 'development') {
 
-// error handlers
-
-//
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-////
-//var bodyParser = require('body-parser');
-//var routes = require('./routes/index');
-//
-//var users = require('./routes/users');
-//// view engine setup
-//
-//app.set('views', path.join(__dirname, 'views'));
-//
-//app.set('view engine', 'ejs');
-//// uncomment after placing your favicon in /public
-////app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use('/', routes);
-//
-//app.use('/users', users);
-//
-//// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  var err = new Error('Not Found');
-//  err.status = 404;
-//  next(err);
-//});
-//if (app.get('env') === 'development') {
-//    app.use(function(err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//      message: err.message,
-//      error: err
-//    });
-//  });
-//}
-//
-//
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function(err, req, res, next) {
-//  res.status(err.status || 500);
-//  res.render('error', {
-//    message: err.message,
-//    error: {}
-//  });
-//});
-
-
+    app.use(logger('dev',{immediate:true}));
+}else{
+    app.use(logger('default'));
+}
 
 //module.exports = app;
